5222611545345a48ee3dea0c290ebf45df687368
Adrian0junio <Saasresumo@outlook.com>
2025-06-03 18:22:49 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Navigate to="/login\" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
